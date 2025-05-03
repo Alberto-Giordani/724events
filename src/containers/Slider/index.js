ed324df3fc5b8f2f7d4551fd6b5e04be9ef7b3f3
Alberto Giordani <albertogiordani@ymail.com>
2025-05-03 15:36:18 +0200

    Modification key boutons radio

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -38,9 +38,9 @@ const Slider = () => {
           </div>
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {byDateDesc.map((evt, radioIdx) => (
+              {byDateDesc.map((_, radioIdx) => (
                 <input
-                  key={`${evt.title}-${evt.date}`}
+                  key={`${event.title}-${event.date}`}
                   type="radio"
                   name="radio-button"
                   checked={index === radioIdx}
