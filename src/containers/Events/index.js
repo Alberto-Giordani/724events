bed1afa7cd2f6658085013f51950e293ca4539d6
Alberto Giordani <albertogiordani@ymail.com>
2025-05-04 09:17:29 +0200

    Correction bug EventList

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,24 +13,20 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = (
-    (!type
-      ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
-    if (
-      (currentPage - 1) * PER_PAGE <= index &&
-      PER_PAGE * currentPage > index
-    ) {
-      return true;
-    }
-    return false;
-  });
+  const filteredByType = !type
+    ? data?.events || []
+    : (data?.events || []).filter((event) => event.type === type);
+  const pageNumber = Math.ceil(filteredByType.length / PER_PAGE);
+  const filteredEvents = filteredByType.slice(
+    (currentPage - 1) * PER_PAGE,
+    currentPage * PER_PAGE
+  );
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
-  const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
+
   const typeList = new Set(data?.events.map((event) => event.type));
   return (
     <>
@@ -62,7 +58,7 @@ const EventList = () => {
           <div className="Pagination">
             {[...Array(pageNumber || 0)].map((_, n) => (
               // eslint-disable-next-line react/no-array-index-key
-              <a key={n} href="#events" onClick={() => setCurrentPage(n + 1)}>
+              <a key={`page-${n + 1}`} href="#events" onClick={() => setCurrentPage(n + 1)}>
                 {n + 1}
               </a>
             ))}
